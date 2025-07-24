a84ca3ba3b2bf019d90da1f1593d44dc7b2e754e
Mrinank Panda <mrinank.panda@gmail.com>
2025-07-24 10:58:54 -0500

    Initial Commit

---
+++ pantrypal/pantrypalApp.swift
@@ -0,0 +1,17 @@
+//
+//  pantrypalApp.swift
+//  pantrypal
+//
+//  Created by Mrinank Panda on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct pantrypalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pantrypalTests/pantrypalTests.swift
@@ -0,0 +1,16 @@
+//
+//  pantrypalTests.swift
+//  pantrypalTests
+//
+//  Created by Mrinank Panda on 7/24/25.
+//
+
+import Testing
+
+struct pantrypalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
